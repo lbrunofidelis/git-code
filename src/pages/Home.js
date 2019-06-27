84909d70fb757eee0b4f52e8dffdefbca9f5b2e9
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-06-27 12:06:59 -0300

    adicionando sidebar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,10 +2,11 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../styles/App.css';
 import { Link } from 'react-router-dom';
-
+import Sidebar from '../components/Sidebar'
 function Home() {
     return (
         <div className="App">
+            <Sidebar/>
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
