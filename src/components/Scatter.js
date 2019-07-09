5e82cf15adbb2c1c9d42b1d280592e1cdaa83d30
Andre Eduardo <andre.eduardo11@hotmail.com>
2019-07-08 21:50:44 -0300

    configurando scatter

--- src/components/Scatter.js
+++ src/components/Scatter.js
@@ -2,80 +2,97 @@ import React, { Component } from 'react';
 import { Scatter } from 'react-chartjs-2';
 import { defaults } from 'react-chartjs-2';
 import '../styles/App.css';
+import UFs from '../data/regiao'
+import gera_cor from '../funcoes/geraCor'
 defaults.global.defaultFontStyle = 'bold';
 
 defaults.global.defaultFontSize = 15;
-const data = {
+var data = {
 
 
     datasets: [
-        {
-            label: ["DF"],
-            fill: false,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: 'rgba(75,132,197,0.85)',
-            pointBorderWidth: 10,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [
-                { x: 20, y: 20 },
-                { x: 59, y: 49 },
-                { x: 80, y: 90 },
-
-            ],
-
-        },
-
-        {
-            label: ["GO"],
-            fill: false,
-            backgroundColor: 'rgb(255, 0, 160)',
-            pointBorderColor: 'rgb(255, 0, 160)',
-            pointBackgroundColor: 'rgba(75,132,197,0.85)',
-            pointBorderWidth: 10,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: [
-                { x: 20, y: 10 },
-            ]
-
-        },
-
-
+       
     ]
 
 
 };
 
-const options = {
 
-    title: {
-        display: true,
-        text: "renda x escolar"
-    },
-    tooltips: {
-        callbacks: {
-            label: function (tooltipItem, data) {
-                var label = data.datasets[tooltipItem.datasetIndex].label || '';
-                return label;
-            }
-        }
-    }
-
-};
 
 
 class ScatterGraph extends Component {
+  constructor(){
+		super()
+		this.state = {
+			estados:[]
+		}
+		
+  }
+
     render() {
+
+      {
+        var uf = []
+        var renda=[]
+        var escolar=[]
+        for (var key in UFs["UFs"]){
+        
+        uf.push((UFs['UFs'][key]['label']))
+        renda.push(UFs['UFs'][key]['renda_media'])
+        escolar.push(UFs['UFs'][key]['escolaridade_media'])
+        
+      }
+      var cor;
+      var j= 0;
+      for( var aux  in uf ){
+        
+        cor = gera_cor();
+        data.datasets.push(
+          
+          {label:`${uf[aux]}`,
+          backgroundColor:  `${cor}` ,
+          pointBorderColor:  `${cor}`,
+          pointBackgroundColor:  `${cor}`,
+          pointBorderWidth: 10,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor:  `${cor}`,
+          pointHoverBorderColor:  `${cor}`,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: [
+            { x: Math.round(renda[j]*100)/100, y:  Math.ceil(escolar[j]*100)/100},
+        ]
+        },
+           
+        )
+        j++;
+      }
+      var options = {
+
+        title: {
+            display: true,
+            text: "Renda x Escolaridade"
+        },
+        
+        tooltips: {
+            callbacks: {
+                
+                label: function (tooltipItem, data) {
+                    var label = data.datasets[tooltipItem.datasetIndex].label || '';
+                          label = label+" X: "+data.datasets[tooltipItem.datasetIndex].data[0].x + " Y:"+data.datasets[tooltipItem.datasetIndex].data[0].y
+                    return label;
+                }
+            }
+        }
+    
+    };
+    for( var code in data.datasets){
+      console.log(data.datasets[code].data[0])
+    }
+    
+      }
+      
         return (
             <Scatter data={data} options={options} />
         )
